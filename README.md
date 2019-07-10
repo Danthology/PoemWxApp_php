6ad55531ff3e82275cb966bed54f25e261b5976a
Danthology <397053880@qq.com>
2019-07-10 15:58:16 +0800

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## 重邮诗词大会微信小程序
+
+[前端地址](https://github.com/Azurak/PoemWxApp)
+
+![qrcode](qrcode.jpg)
--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
-Route::get('test','RedisController@test');
+
 Route::get('wxLogin','LoginController@login')->middleware('wxcheck:login');
 Route::get('wxSubmitUserInfo','LoginController@person')->middleware('wxcheck:keep');
 Route::get('wxGetUserInfo','MsgController@wxGetUserInfo')->middleware('wxcheck:keep');
@@ -25,5 +25,3 @@ Route::get('wxGetQuestions','PoemController@getpoem')->middleware('wxcheck:keep'
 Route::get('wxSubmitAnswer','PoemController@answer')->middleware('wxcheck:keep');
 Route::get('wxGetDailyReview','PoemController@review')->middleware('wxcheck:keep');
 Route::get('wxGetPractice','PracticeController@getpoem')->middleware('wxcheck:keep');
-Route::get('ganshi','HighController@test');
-Route::get('ganbu','HighController@test2');
